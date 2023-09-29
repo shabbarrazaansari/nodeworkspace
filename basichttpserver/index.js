b372f1fa1be77c9b5f4f9deb4f8f8afea379a3bc
shabbarrazaansari <shabbaransari98@gmail.com>
2023-09-29 16:20:45 +0530

    add some url

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,9 +1,18 @@
 const http = require('http');
 const port = 4000;
 function requestHandler(req,res){
-    console.log(req.url);
-    res.end("shabbar Ansari");
+    if (req.url === '/home') {
+        res.end("Welcome home");
+    } else if (req.url === '/about') {
+        res.end("Welcome to About Us page");
+    } else if (req.url === '/node') {
+        res.end("Welcome to my Node Js project");
+    } else {
+        // Handle other URLs or send a default response
+        res.end("Page not found");
+    }
 }
+
 const server = http.createServer(requestHandler);
 server.listen(port,function(err){
     if(err){
