4344019df8d8bdf0e459ac6acc7adce8d89b820d
shabbarrazaansari <shabbaransari98@gmail.com>
2023-10-21 16:56:45 +0530

    added views and cleaning the code

--- httpusingexpress/index.js
+++ httpusingexpress/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 const bodyParser = require('body-parser');
 
@@ -10,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>page not found<h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 
--- httpusingexpress/routes/admin.js
+++ httpusingexpress/routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    //res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
   });
   
   router.post('/product', (req, res, next) => {
